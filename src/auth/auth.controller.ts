baf6c29a184a3f2222f63844eca6eb85ecd2b7a6
devaPOC <devat3204@gmail.com>
2025-11-16 00:13:42 +0530

    feat: implement user authentication with signup and signin functionality, add validation and DTOs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from './auth.service';
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
 	constructor(private AuthService: AuthService) { }
 
 	@Post('signup')
-	signup() {
-		return this.AuthService.signup()
+	signup(@Body() dto: AuthDto) {
+		return this.AuthService.signup(dto)
 	}
 
 	@Post('signin')
-	signin() {
-		return this.AuthService.signin()
+	signin(@Body() dto: AuthDto) {
+		return this.AuthService.signin(dto)
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,54 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { DbService } from "src/db/db.service";
-
-@Injectable({})
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+@Injectable()
 export class AuthService {
 	constructor(private db: DbService) { }
-	signup() {
-		return 'I have signed up'
+	async signup(dto: AuthDto) {
+		// generate the password
+		const hash = await argon.hash(dto.password)
+		try {
+
+			// save the new user to the db
+			const user = await this.db.user.create({
+				data: {
+					email: dto.email,
+					hash
+				}
+			})
+			return { id: user.id, email: user.email, createdAt: user.createdAt }
+
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code == "P2002") {
+					throw new ForbiddenException('User already exists')
+				}
+			}
+			throw error
+
+		}
+		// return the saved user
 	}
 
-	signin() {
-		return 'I have signed in'
-	}
+	async signin(dto: AuthDto) {
+		// find user by email
+		const user = await this.db.user.findUnique({
+			where: {
+				email: dto.email
+			}
+		})
+		if (!user)
+			throw new ForbiddenException(
+				'Invaild Email/Password'
+			);
+		const pwMatch = await argon.verify(user.hash, dto.password)
+		if (!pwMatch)
+			throw new ForbiddenException(
+				'Invaild Email/Password'
+			);
 
+		return { id: user.id, firstName: user.firstName, lastName: user.lastName, email: user.email }
+	}
 }
